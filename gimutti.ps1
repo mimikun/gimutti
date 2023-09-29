f0cb6dce98709daddc400d724d6a8b76994a11b8
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-29 21:33:29 +0900

    Initial commit

---
+++ gimutti.ps1
@@ -0,0 +1,22 @@
+# TODO: 義務ッチ用スクリプト
+
+# Set your browser fullpath
+$browser = Join-Path -Path $env:LOCALAPPDATA -ChildPath Vivaldi\Application\vivaldi.exe
+# Set your main SNS URL
+$sns_url = "https://mstdn.mimikun.jp/deck/getting-started"
+# Set your Twitch URL"
+$twitch_url = "https://twitch.com/mimikun_8"
+# Set your favorite pages
+# e.g. paimon.moe, Teyvat map
+$fav_urls = @(
+    "https://paimon.moe",
+    "https://act.hoyolab.com/ys/app/interactive-map/index.html?lang=ja-jp#/map/2?shown_types=45&center=2008.50,-1084.00&zoom=-3.00",
+)
+# Set your Streaming software fullpath
+# TODO: OBS fullpath
+$streaming_soft = Join-Path 
+# Obsidian.exe fullpath
+# TODO: Obsidian fullpath
+$obsidian_app = Join-Path
+
+# main
